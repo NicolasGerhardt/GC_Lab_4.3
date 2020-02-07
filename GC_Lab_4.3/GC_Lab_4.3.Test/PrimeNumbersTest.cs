2b3e2145a3bfde53f7fa0e3e750a62b9311b4c2b
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-07 10:42:23 -0500

    Finished Prime numbers class using TDD

---
+++ GC_Lab_4.3/GC_Lab_4.3.Test/PrimeNumbersTest.cs
@@ -0,0 +1,99 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GC_Lab_4._3.Test
+{
+    [TestClass]
+    public class PrimeNumbersTest
+    {
+        [TestMethod]
+        public void GetFirstPrimeNumber()
+        {
+            //-- Arrange
+            var primeNumbers = new PrimeNumbers();
+            var expected = 2;
+
+            //-- Act
+            int actual = primeNumbers.GetNPrime(1);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Get10thPrimeNumber()
+        {
+            //-- Arrange
+            var primeNumbers = new PrimeNumbers();
+            var expected = 29;
+
+            //-- Act
+            int actual = primeNumbers.GetNPrime(10);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Number2IsPrime()
+        {
+            //-- Arrange
+            var primeNumbers = new PrimeNumbers();
+            bool expected = true;
+
+            //-- Act
+            bool actual = primeNumbers.IsNumberPrime(2);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        [TestMethod]
+        public void NumberNegitive13IsPrime()
+        {
+            //-- Arrange
+            var primeNumbers = new PrimeNumbers();
+            bool expected = true;
+
+            //-- Act
+            bool actual = primeNumbers.IsNumberPrime(-13);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void NumberNegitive15IsNotPrime()
+        {
+            //-- Arrange
+            var primeNumbers = new PrimeNumbers();
+            bool expected = false;
+
+            //-- Act
+            bool actual = primeNumbers.IsNumberPrime(-15);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Number21IsNotPrime()
+        {
+            //-- Arrange
+            var primeNumbers = new PrimeNumbers();
+            bool expected = false;
+
+            //-- Act
+            bool actual = primeNumbers.IsNumberPrime(21);
+
+            //-- Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
---
+++ GC_Lab_4.3/GC_Lab_4.3/PrimeNumbers.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GC_Lab_4._3
+{
+    public class PrimeNumbers
+    {
+        private List<int> knownPrimes;
+
+        public PrimeNumbers()
+        {
+            knownPrimes = new List<int>();
+            for (int i = 2; i < 10; i++)
+            {
+                if (IsNumberPrime(i))
+                {
+                    knownPrimes.Add(i);
+                }
+            }
+        }
+
+
+        public int GetNPrime(int num)
+        {
+            int index = num - 1;
+            while (knownPrimes.Count <= index)
+            {
+                int lastIndesOflastKnowPrimes = knownPrimes.Count - 1;
+                int lastKnownPrime = knownPrimes[lastIndesOflastKnowPrimes];
+                int nextPrime = CalcNextPrime(lastKnownPrime);
+                knownPrimes.Add(nextPrime);
+            }
+            int prime = knownPrimes[index];
+
+            return prime;
+        }
+
+        private int CalcNextPrime(int num)
+        {
+            int currentNumber = num + 1;
+            while (!IsNumberPrime(currentNumber))
+            {
+                currentNumber++;
+            }
+
+            return currentNumber;
+        }
+
+        public bool IsNumberPrime(int num)
+        {
+            // all calculations are done with positive numbers
+            num = Math.Abs(num);
+
+            for (int i = 2; i < num; i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
