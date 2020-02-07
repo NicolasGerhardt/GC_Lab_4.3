f0e1ba36e0687efbdd9cc6c60619e83a27aacb89
Nic Gerhardt <NicolasGerhardt@quickenloans.com>
2020-02-07 09:47:23 -0500

    inital commit.

---
+++ GC_Lab_4.3/GC_Lab_4.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GC_Lab_4._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
